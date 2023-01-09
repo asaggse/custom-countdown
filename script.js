6310d86f92864ddf36d9a5590e767679ba825c52
Alessandro Saggese <asaggese@gmail.com>
2023-01-09 22:57:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,30 @@ const setDateButton = document.getElementById("set-date-button");
 let targetDate;
 
 function updateCountdown() {
-  const currentDate = new Date();
-  const timeLeft = targetDate - currentDate;
+    const currentDate = new Date();
+    const timeLeft = targetDate - currentDate;
 
-  if (timeLeft < 0) {
-    // Countdown is complete
-    clearInterval(intervalId);
-    return;
-  }
+    if (timeLeft < 0) {
+        // Countdown is complete
+        clearInterval(intervalId);
+        return;
+    }
 
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  daysElement.innerHTML = days;
-  hoursElement.innerHTML = hours;
-  minutesElement.innerHTML = minutes;
-  secondsElement.innerHTML = seconds;
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
 }
 
-setDateButton.addEventListener("click", function() {
-  targetDate = new Date(targetDateInput.value);
-  updateCountdown();
-  intervalId = setInterval(updateCountdown, 1000);
+setDateButton.addEventListener("click", function () {
+    targetDate = new Date(targetDateInput.value);
+    updateCountdown();
+    intervalId = setInterval(updateCountdown, 1000);
 });
